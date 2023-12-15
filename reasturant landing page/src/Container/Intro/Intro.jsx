855c80df3bb4976ce9907616fe358fcd0211b973
akshat <akshatbhatya@gmail.com>
2023-12-15 21:15:20 +0530

    change in itro

--- reasturant landing page/src/Container/Intro/Intro.jsx
+++ reasturant landing page/src/Container/Intro/Intro.jsx
@@ -6,7 +6,7 @@ import { Meal } from '../../assets/index'
 const Intro = () => {
   return (
     <div className='app__into-container'>
-      <video src={Meal} autoPlay="true" controls allowFullScreen width={"100%"}/>
+      <video src={Meal} autoPlay={true} controls allowFullScreen width={"100%"}/>
     </div>
   )
 }
