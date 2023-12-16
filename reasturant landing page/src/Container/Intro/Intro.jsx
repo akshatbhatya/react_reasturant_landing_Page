07e1a468b0196add6bc3c7da6bbe991ad467d8c4
akshat <akshatbhatya@gmail.com>
2023-12-16 12:03:41 +0530

    changs in into section

--- reasturant landing page/src/Container/Intro/Intro.jsx
+++ reasturant landing page/src/Container/Intro/Intro.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 import "./Intro.css"
 import { Meal } from '../../assets/index'
+import { FaRegPlayCircle } from "react-icons/fa";
+import { FaRegCirclePause } from "react-icons/fa6";
+
 
 const Intro = () => {
+  const VideoRef = useRef();
+  const [playvideo, setPlayVideo] = useState(true)
+  const videoHandler = () => {
+    setPlayVideo((prev) => !prev)
+    if (playvideo) {
+      VideoRef.current.pause()
+
+    }
+    else {
+      VideoRef.current.play()
+    }
+  }
+
+  console.log(playvideo);
   return (
-    <div className='app__into-container'>
-      <video src={Meal} autoPlay={true} controls allowFullScreen width={"100%"}/>
+    <div className='parent'>
+      <div className='app__into-container'>
+
+        <div className="app__into-video">
+
+          <video
+            src={Meal}
+            ref={VideoRef}
+            autoPlay={true}
+            controls={false}
+            allowFullScreen
+            muted
+            loop
+            width={"100%"} />
+          <div className="app__intro--videobtn flex__center ">
+            {
+              playvideo ? <><FaRegCirclePause onClick={videoHandler} size={50} /></> : <><FaRegPlayCircle onClick={videoHandler} size={50} /></>
+            }
+
+          </div>
+        </div>
+
+      </div>
+
     </div>
+
   )
 }
 
